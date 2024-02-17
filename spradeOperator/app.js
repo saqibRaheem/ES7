5dcfbe87cffb4b7f064b1e0255d61ff3791e93ec
saqibRaheem <85112626+saqibRaheem@users.noreply.github.com>
2024-02-17 15:26:47 +0500

    add sprate operator

---
+++ spradeOperator/app.js
@@ -0,0 +1,20 @@
+// sprade operator ... is allows an iterable such as an array or string to be expanded into seprate elements 
+
+// let numbers = [1, 2, 3, 4, 5]
+// console.log(numbers);
+// let maxNum = Math.max(2,3,4,9) // 9
+// let maxNum = Math.max(numbers) // NaN
+// let maxNum = Math.max(...numbers) // sprade sentex 
+// console.log(maxNum);
+
+
+// let text = "saqib raheem";
+// let sep = [text] // simple array
+// let sep = [...text] // each character seprate
+// console.log(sep);
+
+// let fruits = ['apple','orange','mango'];
+// let vegetables = ['patatoes','onion','tamato']
+// let add = [fruits,vegetables]
+// let add = [...fruits,...vegetables]
+// console.log(add);
